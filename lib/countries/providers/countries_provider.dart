a7bd5cfd3ef154ebfd71150c9635a7e05e983baf
Ing Oscar Lobo <oscarescalando@gmail.com>
2020-09-26 14:58:39 -0400

    ajustes provider countries

--- lib/countries/providers/countries_provider.dart
+++ lib/countries/providers/countries_provider.dart
@@ -16,6 +16,17 @@ class _CountriesProvider {
 
     return opciones;
   }
+
+  Future<String> getMyCountry(String id) async {
+    final resp = await rootBundle.loadString('data/countries_list.json');
+    Map dataMap = json.decode(resp);
+    opciones = dataMap['countries'];
+
+    var country = opciones.where((e) => e['id'] == id);
+    print(country);
+    //return country.['name'].toString();
+    //Map filtro = dataMap.entries.firstWhere((element) => false)
+  }
 }
 
 final menuProvider = new _CountriesProvider();
